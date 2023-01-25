fec42ec9d2ff91ebd6b7c613adff0eac94e087fe
Sergii Kukhariev <serge.kukharev@gmail.com>
2023-01-25 18:34:39 -0300

    Final PoC

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,25 @@ import {DefaultApi} from "./generatedClient/api/defaultApi";
 
 const app = express();
 const port = 3000;
-const client = new DefaultApi("https://example.com/api/");
+const client = new DefaultApi("http://localhost:3000");
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-
-    client.getRecipientById("abcdeddd").then(() => {
+    client.getRecipientById("abcdeddd").then((apiResponse) => {
         console.log("Got response!");
-    }).catch(() => {
+        res.send("Got recipient from API! </br>" + JSON.stringify(apiResponse.body.recipient));
+    }).catch((error) => {
         console.log("Request failed!");
+        res.send("Failed getting recipient from API! </br>" + JSON.stringify(error));
+    });
+});
+
+app.get('/recipients/:id', (req, res) => {
+    res.json({
+        "recipient": {
+            "id": req.params.id,
+            "name": "john.doe",
+            "email": "john@example.com"
+        }
     });
 });
 

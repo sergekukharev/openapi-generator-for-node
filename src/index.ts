ac6ab6bc768cf50911b1c9a10f7bae2a61720d64
Sergii Kukhariev <serge.kukharev@gmail.com>
2023-01-25 17:05:34 -0300

    Added some logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import {DefaultApi} from "testClient/api/defaultApi";
+
 console.log("hi!");
+
+const api = new DefaultApi("example.com/api");
+
+api.recipientsIdGet("abcdef").then(()=>{
+    console.log("Got response!");
+});

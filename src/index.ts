7aca1a7781e24644d2e6646fb41fca06ae644ee0
Sergii Kukhariev <serge.kukharev@gmail.com>
2023-01-25 17:56:51 -0300

    Removed hello world file

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import {DefaultApi} from "testClient/api/defaultApi";
-
-console.log("hi!");
-
-const api = new DefaultApi("example.com/api");
-
-api.recipientsIdGet("abcdef").then(()=>{
-    console.log("Got response!");
-});

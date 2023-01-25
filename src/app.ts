26ef2f21b369df4c6037b483979cb101ff1c84dc
Sergii Kukhariev <serge.kukharev@gmail.com>
2023-01-25 17:57:32 -0300

    Basic PoC of the client

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import {DefaultApi} from "./generatedClient/api/defaultApi";
+
 const app = express();
 const port = 3000;
+const client = new DefaultApi("https://example.com/api/");
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+
+    client.recipientsIdGet("abcdeddd").then(() => {
+        console.log("Got response!");
+    }).catch(() => {
+        console.log("Request failed!");
+    });
 });
 
 app.listen(port, () => {
